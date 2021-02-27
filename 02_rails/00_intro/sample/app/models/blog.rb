96f76b5abb3d05a4187124159a9b32c2d9968678
diescake <daisuke.kondo@smarthr.co.jp>
2021-02-27 19:24:45 +0900

    modify the sample codes

--- 02_rails/00_intro/sample/app/models/blog.rb
+++ 02_rails/00_intro/sample/app/models/blog.rb
@@ -1,2 +1,15 @@
 class Blog < ApplicationRecord
+  # dependent :destroy をつけると Blog に関連するレコードも一緒に消去する
+  has_many :entries, dependent: :destroy
+  validates :title, presence: true
+  scope :title_search, ->(title) { where('title like :title', title: "%#{title}") }
+  # return by "all" if title is not matched
+  scope :notfound_scope, -> { where(title: '100000').first }
+
+  # 完全に同時リクエストの場合アプリケーションレベルのバリデーションはすり抜けることがある
+  validates :title, uniqueness: true
+
+  def self.notfound_method
+    where(title: '100000').first
+  end
 end
--- 02_rails/00_intro/sample/db/seeds.rb
+++ 02_rails/00_intro/sample/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  b = Blog.create(title: "blog #{n}")
+  Random.rand(1..4).times do |r|
+    b.entries << Entry.create(name: "entry #{r}", body: 'あ' * r)
+  end
+end
